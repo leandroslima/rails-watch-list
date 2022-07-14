a5728a751857ed982db9f16d9f865f29bcd33e00
leandro lima <leandro.desouzalima1@gmail.com>
2022-07-14 14:52:04 -0300

    Bookmarks Controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,15 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
